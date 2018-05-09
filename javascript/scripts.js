a2764695a58f81bcfa6438d78d015de3c33ed49a
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-09 19:46:29 +0200

    change after codereview

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,11 +1,10 @@
 function drawTree(h){
-drawTree();
-  for( var i = 0; i < h; i++){
+  for (var i = 0; i < h; i++) {
     var star = "";
-  
-    for( var j = 0; j<=i ;j++){
+    for (var j = 0; j <= i; j++) {
       star += "*";  
     }
     console.log(star);
   }
 }
+drawTree(12);
